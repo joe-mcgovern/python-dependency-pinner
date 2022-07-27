054752243ddd84894cde48c7149d046b2242fe7a
Joe McGovern <joemcgovern@granular.ag>
2022-07-27 09:42:27 -0500

    Add README and scripts

--- README.md
+++ README.md
@@ -1,13 +1,39 @@
 # Python dependency pinner
 
-Too often, I see dependencies pinned at version `*` in Pipfile and pyproject.toml
-files. This is a terrible practice, and is quite exactly the road to dependency hell. 
-Any time you want to update a single dependency, you end up updating all your dependencies 
+Sometimes, dependencies are pinned at version `*` in Pipfile and pyproject.toml
+files. This is a terrible practice, resulting in dependency hell. 
+Any time you want to update a single dependency, you end up updating _all_ your dependencies 
 to their latest version, which usually includes some backwards incompatible major version 
 bumps. If you have more than one dependency, trying to comprehend all of the changes that
 were just installed and verifying that they all worked is overwhelmingly cumbersome.
+
+I faced this dependency hell several times at my previous job. I wrote these
+scripts so I could pin dependencies any time I came across a project that used
+the `*` dependency version pattern and thus keep my sanity.
 
 So, I wrote two small, untested, no-dependency python scripts to replace the `*` in the 
 dependency file with whatever version is listed for that dependency in the lock file.
 I configured the dependencies to allow _patch_ updates whenever dependencies are updated,
 but not minor or major updates.
+
+These scripts were written at different times and have different output formats.
+Maybe someday I'll get around to making them look similar. 
+
+Disclaimer: I've only run these scripts a handful of times. They're not robust,
+probably have bugs, and won't handle every usecase. Feel free to make suggestions
+or extend them at your own discretion.
+
+## Usage
+
+For pinning pipenv projects, make sure you are in the directory that has the Pipfile
+and Pipfile.lock and then run:
+
+```
+python /path/to/pipfile_pinner.py
+```
+
+For pinning poetry projects, make sure you are in the directory that has the pyproject.toml
+and poetry.lock file and then run:
+```
+python /path/to/poetry_pinner.py
+```
--- poetry_pinner.py
+++ poetry_pinner.py
@@ -1,51 +1,61 @@
-print("Analyzing pyproject.toml...")
-with open("pyproject.toml") as f:
-    pyproject_content = f.read()
+def main():
+    print("Analyzing pyproject.toml...")
+    with open("pyproject.toml") as f:
+        pyproject_content = f.read()
 
-deps = []
-is_dependency_section = False
-pyproject_lines = pyproject_content.splitlines()
-for line_number, line in enumerate(pyproject_lines):
-    if line.startswith("[") and "dependencies" in line:
-        is_dependency_section = True
-    elif line.startswith("["):
-        is_dependency_section = False
-    elif is_dependency_section and line.strip() and "*" in line:
-        package = line.split("=")[0].strip()
-        deps.append(
-            {
-                "name": package,
-                "line_number": line_number,
-            }
-        )
+    deps = []
+    is_dependency_section = False
+    pyproject_lines = pyproject_content.splitlines()
+    for line_number, line in enumerate(pyproject_lines):
+        if line.startswith("[") and "dependencies" in line:
+            is_dependency_section = True
+        elif line.startswith("["):
+            is_dependency_section = False
+        elif is_dependency_section and line.strip() and "*" in line:
+            package = line.split("=")[0].strip()
+            deps.append(
+                {
+                    "name": package,
+                    "line_number": line_number,
+                }
+            )
 
-print("Analayzing poetry.lock...")
-with open("poetry.lock") as f:
-    lock = f.read()
+    print("Analyzing poetry.lock...")
+    with open("poetry.lock") as f:
+        lock = f.read()
 
-lock_deps = {}
-current_name = None
-for line in lock.splitlines():
-    if line.startswith("name"):
-        ugly_name = line.split(" = ")[1]
-        current_name = ugly_name.strip('"').strip("'")
-        lock_deps[current_name] = ""
-    elif line.startswith("version"):
-        ugly_version = line.split(" = ")[1]
-        version = ugly_version.strip('"').strip("'")
-        lock_deps[current_name] = version
-    elif line == "[[package]]":
-        current_name = None
+    lock_deps = {}
+    current_name = None
+    for line in lock.splitlines():
+        if line.startswith("name"):
+            ugly_name = line.split(" = ")[1]
+            current_name = ugly_name.strip('"').strip("'")
+            lock_deps[current_name] = ""
+        elif line.startswith("version"):
+            ugly_version = line.split(" = ")[1]
+            version = ugly_version.strip('"').strip("'")
+            lock_deps[current_name] = version
+        elif line == "[[package]]":
+            current_name = None
 
-for dependency in deps:
-    if dependency["name"] in lock_deps:
-        lock_version = lock_deps[dependency["name"]]
-        line = pyproject_lines[dependency["line_number"]]
-        new_line = line.replace("*", f"~{lock_version}")
-        pyproject_lines[dependency["line_number"]] = new_line
-    else:
-        print("Unhandled dependency:", dependency["name"])
+    for dependency in deps:
+        if dependency["name"] in lock_deps:
+            lock_version = lock_deps[dependency["name"]]
+            line = pyproject_lines[dependency["line_number"]]
+            new_line = line.replace("*", f"~{lock_version}")
+            pyproject_lines[dependency["line_number"]] = new_line
+        else:
+            print(
+                "Could not update:",
+                dependency["name"],
+                "(it wasn't found in the lock file. This is likely a bug in "
+                "the code that analyzes the lock file)",
+            )
 
-print("Writing changes...")
-with open("pyproject.toml", "w") as f:
-    f.write("\n".join(pyproject_lines))
+    print("Writing changes...")
+    with open("pyproject.toml", "w") as f:
+        f.write("\n".join(pyproject_lines))
+
+
+if __name__ == "__main__":
+    main()
